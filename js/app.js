2f37e074649f86c03dd99fcafdcce6a148c7a849
Sparklebunny Stevens <jeribeth@gmail.com>
2014-04-10 19:40:53 -0600

    adding js ideas
    no functionality yet, adding js/jquery to get ideas down

--- js/app.js
+++ js/app.js
@@ -12,6 +12,77 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+// ---------------------------------------------------
+	// List of Variables
+	// var game = 
+	// var answer = Math.floor(Math.random()*10)+1);
+	// 	console.log("The answer is " + answer);
+	// var numberOfGuesses = 0;
+	// var guesses = [];
+	// var distance = null;
+	// var previousDistance = null;
+
+
+	$(".game").function getRandomInt() {
+  		return answer;
+	}
+
+	function getGuess() {
+		$("#guessButton").click(game);
+		$("#userGuess").keydown(function (enter) {
+			if (enter.keyCode == 13) {
+				game();
+			}
+		});
+	}
+
+	$("guessList").append(function) {
+		if ("#userGuess" > answer);
+			return "Your answer is too high";
+		if ("#userGuess" < answer);
+			return "Your answer is too low";
+		else if ("#userGuess" == answer);
+			return "You're Right! Nice job!";
+	}
+
+
+
+
+	getGuess();
+
+	function game() {
+    
+    $('#newgame').click(function (e) {
+        e.preventDefault();
+        var answer = Math.floor((Math.random() * 100) + 1);
+        	console.log(answer);
+        var numberOfGuesses = 0;
+        var guesses = [];
+        var distance = null;
+        var previousDistance = null;
+        // $('#hint').html('');
+        $('#guess').val('');
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
