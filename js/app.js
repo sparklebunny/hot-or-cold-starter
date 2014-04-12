9baf6c85965961cdc618f17bc32619e87dfb4862
Sparklebunny Stevens <jeribeth@gmail.com>
2014-04-11 22:47:40 -0600

    Added functionality to game
    basic functionality with alerts for hot, cold, correct

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -15,71 +15,75 @@ $(document).ready(function(){
 // ---------------------------------------------------
 	// List of Variables
 	// var game = 
-	// var answer = Math.floor(Math.random()*10)+1);
-	// 	console.log("The answer is " + answer);
-	// var numberOfGuesses = 0;
-	// var guesses = [];
-	// var distance = null;
-	// var previousDistance = null;
-
-
-	$(".game").function getRandomInt() {
-  		return answer;
-	}
-
-	function getGuess() {
-		$("#guessButton").click(game);
-		$("#userGuess").keydown(function (enter) {
-			if (enter.keyCode == 13) {
-				game();
-			}
-		});
-	}
-
-	$("guessList").append(function) {
-		if ("#userGuess" > answer);
-			return "Your answer is too high";
-		if ("#userGuess" < answer);
-			return "Your answer is too low";
-		else if ("#userGuess" == answer);
-			return "You're Right! Nice job!";
-	}
-
-
-
-
-	getGuess();
+	var answer = Math.floor(Math.random()*10)+1;
+		console.log("The answer is " + answer);
+	var numberOfGuesses = 0;
+	var distance = null;
+	var previousDistance = null;
 
 	function game() {
-    
-    $('#newgame').click(function (e) {
-        e.preventDefault();
-        var answer = Math.floor((Math.random() * 100) + 1);
-        	console.log(answer);
-        var numberOfGuesses = 0;
-        var guesses = [];
-        var distance = null;
-        var previousDistance = null;
-        // $('#hint').html('');
-        $('#guess').val('');
-    });
-}
+	    $("#new").click(function (event) {
+	        // event.preventDefault();
+	        answer = Math.floor((Math.random() * 100) + 1);
+	        	console.log(answer);
+			numberOfGuesses = 0;
+	        guesses = [];
+	        distance = null;
+	        previousDistance = null;
+	        $("#guess").val(''); 
+	    });
+	}
+
+
+	// $(".game").function getRandomInt() {
+ //  		return answer;
+	// }
+
+	$("#guessButton").click(processGuess); 
+
+	function processGuess() {
+		var guess = $("#userGuess").val();
+		var numberGuess = parseInt(guess, 10);
+		if (numberGuess > answer) {
+			alert("You're hot. Try again.")
+		}
+		if (numberGuess < answer) {
+			alert("You're cold. Try again.")
+		}
+		else if (numberGuess == answer) {
+			alert("YOU'RE CORRECT! Way to go! Choose NEW GAME to play again.")
+		}
+		$("#count").html(++numberOfGuesses);
+		$("#guessList").append('<p>' + guess + '</p>');
+
+	}
+
+
+
+	// function getGuess() {
+	// 	$("#guessButton").click(game);
+	// 	$("#userGuess").keydown(function (enter) {
+	// 		if (enter.keyCode == 13) {
+	// 			game();
+	// 		}
+	// 	});
+	// }
+
+	// $("guessList").append(function) {
+	// 	if ("#userGuess" > answer);
+	// 		return "Your answer is too high";
+	// 	if ("#userGuess" < answer);
+	// 		return "Your answer is too low";
+	// 	else if ("#userGuess" == answer);
+	// 		return "You're Right! Nice job!";
+	// }
 
 
 
 
+	// getGuess();
 
-
-
-
-
-
-
-
-
-
-
-
+	
 
 
 
